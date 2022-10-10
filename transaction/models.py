52e6b3723a13e76474d7994d48c3729f8d277781
Andrew Jeremy <Andrewjeremy12345@gmail.com>
2022-10-11 00:02:43 +0700

    added initial models

--- transaction/models.py
+++ transaction/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.utils import timezone
+from wallet.models import Wallet
 
-# Create your models here.
+class Transaction(models.Model):
+  wallet = models.ForeignKey(Wallet, on_delete=models.CASCADE)
+  date = models.DateField(default=timezone.now)
+  amount = models.IntegerField()
+  note = models.TextField()
