01534e2a5e598ab1d4a5037f25416b7bc268dbe5
Nayyara Airlangga <nayyara.airlangga@gmail.com>
2022-10-11 02:25:23 +0700

    Add support for env variables

--- manage.py
+++ manage.py
@@ -3,9 +3,15 @@
 import os
 import sys
 
+from dotenv import load_dotenv
+
 
 def main():
     """Run administrative tasks."""
+
+    # Load environment variables
+    load_dotenv(os.path.join(os.path.dirname(__file__), '.env'))
+
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'duitku.settings')
     try:
         from django.core.management import execute_from_command_line
