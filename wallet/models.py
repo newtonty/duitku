a2e7dc3bc9b65291737c543482b5b3a972c61d83
Andrew Jeremy <Andrewjeremy12345@gmail.com>
2022-10-13 16:31:52 +0700

    removed apps

--- wallet/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import User
-
-class Wallet(models.Model):
-  user = models.ForeignKey(User, on_delete=models.CASCADE)
-  title = models.CharField(max_length=100)
-  balance = models.IntegerField()
