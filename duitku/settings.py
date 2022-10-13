2e2559f4855ef80d5e121e5895d11003fdf95488
Nayyara Airlangga <nayyara.airlangga@gmail.com>
2022-10-13 18:14:28 +0700

    Remove initial apps

--- duitku/settings.py
+++ duitku/settings.py
@@ -49,9 +49,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'auth',
-    'wallet',
-    'transaction',
 ]
 
 MIDDLEWARE = [
